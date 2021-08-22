c96f31784aba9f90227034be8ea5e62d6bdec6d9
Gst <leegxf@gmail.com>
2021-08-22 15:48:13 +0800

    密码框绑定密码、标记扩展

--- MarkupExtension/MainWindow.xaml.cs
+++ MarkupExtension/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PasswordBind
+namespace MarkupExtension
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,7 +23,6 @@ namespace PasswordBind
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new ViewModel();
         }
     }
 }
---
+++ PasswordBind/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MvvmLibary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordBind
+{
+    class ViewModel : BindableBase
+    {
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+
+        private string account;
+
+        public string Account
+        {
+            get { return account; }
+            set { SetProperty(ref account, value); }
+        }
+
+        private DelegateCommand loginCommand;
+
+        public DelegateCommand LoginCommand => loginCommand ?? (loginCommand = new DelegateCommand(ExcuteLoginCommand));
+
+        void ExcuteLoginCommand()
+        {
+            Console.WriteLine("Account:{0}", Account);
+            Console.WriteLine("Password:{0}", Password);
+        }
+    }
+}
