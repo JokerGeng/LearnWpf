a90aaf004ad1945fe6b453ade7dc6d8ce9a519ec
Gst <leegxf@gmail.com>
2021-09-15 22:54:10 +0800

    path警告图

--- Drawing/MainWindow.xaml.cs
+++ Drawing/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TransformDemo
+namespace Drawing
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
