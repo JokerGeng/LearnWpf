d14ffc5720a2c87899c4ab18a671095afa47eb43
Gst <leegxf@gmail.com>
2021-09-22 22:20:02 +0800

    animation

---
+++ LoadingAnimation/Loading.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoadingAnimation
+{
+    /// <summary>
+    /// Loading.xaml 的交互逻辑
+    /// </summary>
+    public partial class Loading : Window
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            this._loading.Visibility = Visibility.Visible;
+        }
+
+        private void HideButton_Click(object sender, RoutedEventArgs e)
+        {
+            this._loading.Visibility = Visibility.Hidden;
+        }
+    }
+}
