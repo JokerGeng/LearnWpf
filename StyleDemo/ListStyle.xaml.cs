9c24c03d492da6f594f87a372c4356d4d45cc28e
Gst <leegxf@gmail.com>
2021-10-12 22:48:45 +0800

    样式

---
+++ StyleDemo/ListStyle.xaml.cs
@@ -0,0 +1,34 @@
+﻿using StoreDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StyleDemo
+{
+    /// <summary>
+    /// ListStyle.xaml 的交互逻辑
+    /// </summary>
+    public partial class ListStyle : Window
+    {
+        public ListStyle()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdGetProducts_Click(object sender, RoutedEventArgs e)
+        {
+            var products = new StoreDB().GetProducts();
+            lstProducts.ItemsSource = products;
+        }
+    }
+}
