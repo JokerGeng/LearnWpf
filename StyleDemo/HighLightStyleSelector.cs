ede0b9b30c1936aab5ccf61df510ee7f5eaeeb60
Gst <leegxf@gmail.com>
2021-10-20 21:30:16 +0800

    merge

--- StyleDemo/HighLightStyleSelector.cs
+++ StyleDemo/HighLightStyleSelector.cs
@@ -10,22 +10,27 @@ using System.Windows.Controls;
 
 namespace StyleDemo
 {
-    class HighLightStyleSelector:StyleSelector
+    class HighLightStyleSelector : StyleSelector
     {
         public Style Default { get; set; }
 
         public Style HighLightStyle { get; set; }
 
+        /// <summary>
+        /// 属性
+        /// </summary>
         public string EvaluteProperty { get; set; }
 
+        /// <summary>
+        /// 属性值
+        /// </summary>
         public string EvalutePropertyValue { get; set; }
 
         public override Style SelectStyle(object item, DependencyObject container)
         {
-            //Product product = (Product)item;
             Type type = item.GetType();
             PropertyInfo propertyInfo = type.GetProperty(EvaluteProperty);
-            if(propertyInfo.GetValue(item,null).ToString().Equals(EvalutePropertyValue))
+            if (propertyInfo.GetValue(item, null).ToString().Equals(EvalutePropertyValue))
             {
                 return HighLightStyle;
             }
