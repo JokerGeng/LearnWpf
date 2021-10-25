e41b4516399e7cf8b6e8405080dcaea43ce3944c
Gst <leegxf@gmail.com>
2021-10-25 22:45:34 +0800

    数据模板选择器

---
+++ StyleDemo/HighLightDataTemplateSelector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StyleDemo
+{
+    class HighLightDataTemplateSelector:DataTemplateSelector
+    {
+        public DataTemplate Default { get; set; }
+
+        public DataTemplate HighLightTemplate { get; set; }
+
+        /// <summary>
+        /// 属性
+        /// </summary>
+        public string EvaluteProperty { get; set; }
+
+        /// <summary>
+        /// 属性值
+        /// </summary>
+        public string EvalutePropertyValue { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            Type type = item.GetType();
+            PropertyInfo propertyInfo = type.GetProperty(EvaluteProperty);
+            if (propertyInfo.GetValue(item, null).ToString().Equals(EvalutePropertyValue))
+            {
+                return HighLightTemplate;
+            }
+            return Default;
+        }
+    }
+}
